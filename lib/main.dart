7e61844326df7c60df0fe2da07144b5562505694
Sazidul islam <rabbiking00@gmail.com>
2024-10-27 09:20:24 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:course_online/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CourseOnline());
+}
+
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+import '../features/authentication/presentation/screens/login_screen.dart';
+
+class AppRoute {
+  static String loginScreen = "/loginScreen";
+
+
+  static String getLoginScreen() => loginScreen;
+
+  static List<GetPage> routes = [
+    GetPage(name: loginScreen, page: () => const LoginScreen()),
+
+  ];
+}
