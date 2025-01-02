3c75278e24b3df4d51121b2a2649c0ea26986104
Sazidul islam <rabbiking00@gmail.com>
2025-01-02 11:29:21 +0600

    websoket funtion add

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,11 +5,9 @@ import '../features/authentication/presentation/screens/login_screen.dart';
 class AppRoute {
   static String loginScreen = "/loginScreen";
 
-
   static String getLoginScreen() => loginScreen;
 
   static List<GetPage> routes = [
-    GetPage(name: loginScreen, page: () => const LoginScreen()),
-
+    GetPage(name: loginScreen, page: () => const HomeScreen()),
   ];
 }
